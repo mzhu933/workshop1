88e8bbf2f9021afcd828919a63e38104cbc14a4c
selina <mzhu933@aucklanduni.ac.nz>
2025-01-15 09:10:22 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(500, 500);
+  background(220);
+
+  for (x = 0; x < 30; x++) {
+    drawEllipse(random(0, width), random(0, height), random(30, 60));
+  }
+}
+
+function drawEllipse(x, y, size) {
+  fill(150, 230, 250, 150); 
+  ellipse(x, y, size, size+30);
+}
